69d4312788568675bc5ed372355658d3dedd2649
Joe <96437590+JoeDLong@users.noreply.github.com>
2022-04-23 16:25:48 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+var main = document.querySelector('.home');
+var nav = document.querySelector('.nav');
+
+var offset = main.offsetHeight - nav.offsetHeight;
+
+window.onscroll = function () {
+    if (window.pageYOffset > offset) {
+        nav.classList.remove('bottom-nav');
+        nav.classList.add('top-nav');
+    }
+    else {
+        nav.classList.add('bottom-nav');
+        nav.classList.remove('top-nav');
+    }
+}
